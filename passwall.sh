546596c311bb2be1d15f10c1cbed0603a9585a83
ligus <75608937+ligusx@users.noreply.github.com>
2024-04-11 21:07:10 +0800

    Add files via upload

--- passwall.sh
+++ passwall.sh
@@ -2,28 +2,34 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 # --------------------------------------------------------------
-#	使用说明：加在openwrt上系统--计划任务里添加定时运行，如0 9 * * * ash /root/passwall.sh
+#	使用说明：加在openwrt上系统--计划任务里添加定时运行，如0 9 * * * ash /root/cf
 #	*解释：9点0分运行一次。
 # --------------------------------------------------------------
 
 echo -e "开始测速..."
+
+# cd到脚本所在位置
+cd `dirname $0`
+
+# 检测是否有特定文件
 NOWIP=$(head -1 nowip_hosts.txt)
 
+# 停止passwall
 /etc/init.d/passwall stop
-sleep 10
+
 # 这里可以自己添加、修改 CloudflareST 的运行参数
+./CloudflareST -n 1000 -url https://st.1275905.xyz/ -tl 240 -tll 45 -o "result_hosts.txt"
 
-./CloudflareST -url https://cfspeed1.kkiyomi.top/200mb.bin -tl 160 -tll 45 -o "result_hosts.txt"
-
-[[ ! -e "result_hosts.txt" ]] && echo "CloudflareST 测速结果 IP 数量为 0，跳过下面步骤..."
+# 检测测速结果文件，没有数据会重启passwall并退出脚本
+[[ ! -e "result_hosts.txt" ]]
 
 BESTIP=$(sed -n "2,1p" result_hosts.txt | awk -F, '{print $1}')
 if [[ -z "${BESTIP}" ]]; then
 	echo "CloudflareST 测速结果 IP 数量为 0，跳过下面步骤..."
-        /etc/init.d/passwall start
-        sleep 10
+	/etc/init.d/passwall start
 	exit 0
 fi
+
 echo ${BESTIP} > nowip_hosts.txt
 echo -e "\n旧 IP 为 ${NOWIP}\n新 IP 为 ${BESTIP}\n"
 
@@ -46,10 +52,13 @@ fi
 
 # 读取nowip_hosts.txt文件中的内容并处理
 while IFS= read -r line; do
-    # 替换/etc/config/passwall文件中的option address字段中的引号中的文本
+
+# 替换/etc/config/passwall文件中的option address字段中的引号中的文本
     sed -i "s/option address '.*'/option address '$line'/" "$passwall_file"
 done < "$nowip_file"
 
 echo "替换完成"
+
+# 删除测速结果文件并启动passwall
 rm -rf result_hosts.txt
 /etc/init.d/passwall start
